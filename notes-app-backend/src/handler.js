7ef16d2a999b49d21a5d21b6dfd42a966883388d
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-22 18:25:51 +0700

    learn to create handler  req,res to create note

--- notes-app-backend/src/handler.js
+++ notes-app-backend/src/handler.js
@@ -1,7 +1,45 @@
 const { nanoid } = require('nanoid');
+const notes = require('./notes');
 
 const addNotHandler = (req, h) => {
+   // This to init data structure will we build
    const  { title, tags, body } = req.payload;
+
+   const id = nanoid(16);
+   const createdAt = new Date().toISOString();
+   const updateAt = createdAt;
+
+   // This will create data at file
+   const newNote = {
+      title, tags, body, id, createdAt, updateAt,
+   };
+
+   notes.push(newNote);
+
+   // This will check isSuccess or not
+   const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+   if (isSuccess) {
+      const response = h.response({
+         status: 'success',
+         message: 'Note successfully created',
+         data: {
+            noteId: id,
+         },
+      });
+
+      response.code(201);
+      return response;
+   }
+
+   // This if not success
+   const response = h.response({
+      status: 'fail',
+      message: 'Note failed to create',
+   });
+
+   response.code(500);
+   return response;
 };
 
 module.exports = addNotHandler;
