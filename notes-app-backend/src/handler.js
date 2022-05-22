b80b95a84195c5cdebca920801a56c95aa8f04d6
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-22 19:26:53 +0700

    learn how to create update data at hapi

--- notes-app-backend/src/handler.js
+++ notes-app-backend/src/handler.js
@@ -73,6 +73,47 @@ response.code(404);
 return response;
 };
 
+// This will update data by Id
+
+const editNoteByIdHandler = (req, h) => {
+   const { id } = req.params;
+
+   const { title, tags, body } = req.payload;
+   const updatedAt = new Date().toISOString();
+
+   const index = notes.findIndex((note) => note.id === id);
+
+   if (index !== -1) {
+      notes[index] = {
+         ...notes[index],
+         title,
+         tags,
+         body,
+         updatedAt,
+      };
+
+      // if success
+   const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+   });
+      response.code(200);
+      return response;
+   }
+   // if not success
+   
+   const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+   });
+   response.code(404);
+   return response;
+};
 
 
-module.exports = { addNotHandler, getAllNotesHandler, getNoteByIdHandler};
+module.exports = { 
+   addNotHandler,
+   getAllNotesHandler,
+   getNoteByIdHandler,
+   editNoteByIdHandler,
+};
--- notes-app-backend/src/routes.js
+++ notes-app-backend/src/routes.js
@@ -1,4 +1,9 @@
-const {addNotHandler, getAllNotesHandler, getNoteByIdHandler } = require("./handler");
+const {
+   addNotHandler,
+   getAllNotesHandler, 
+   getNoteByIdHandler,
+   editNoteByIdHandler, 
+} = require("./handler");
 
 const routes = [
    {
@@ -15,6 +20,11 @@ const routes = [
       method: 'GET',
       path: '/notes/{id}',
       handler: getNoteByIdHandler,
+   },
+   {
+      method: 'PUT',
+      path: '/notes/{id}',
+      handler: editNoteByIdHandler,
    },
 ];
 
